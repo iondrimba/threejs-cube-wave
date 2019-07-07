370dafc15e844012d804f7b21c5d2472cc239eff
Ion D. Filho <iondrimba@gmail.com>
2019-07-07 12:49:47 -0700

    Updates clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
